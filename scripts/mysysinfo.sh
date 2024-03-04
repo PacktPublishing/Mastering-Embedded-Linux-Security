ac9534012f18fcc06ff79ddd5f6e4317f7809a27
mattstonge <matt_st_onge@yahoo.com>
2024-03-03 22:24:24 -0500

    Initial script definitio;n

---
+++ scripts/mysysinfo.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+# mysysinfo.sh
+# By Matt St. Onge
+#
+# This script does a deep introspection of your system 
+# and then generates a report file.
+#
+###################################
+#
+# version 1.00.0  3/3/2024
+#
+# MIT License ( https://opensource.org/license/mit/ )
+#
+###################################
+
+
+###################################
+#
+# Variables
+#
+###################################
+
+
+
+
+
+
+###################################
+#
+# Execution
+#
+###################################
+
+
