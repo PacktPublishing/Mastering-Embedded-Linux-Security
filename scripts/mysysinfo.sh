6416313e94f5300e4caeb6593ae459d89a450f1f
mattstonge <matt_st_onge@yahoo.com>
2024-03-05 17:09:26 -0500

    initial framework for mysysinfo.sh

--- scripts/mysysinfo.sh
+++ scripts/mysysinfo.sh
@@ -20,10 +20,26 @@
 #
 ###################################
 
+# define your product name
+MYPRODNAME="SuperAppliance"
 
+# define your build release version
+MYRELEASEVER=1.00.00
 
+# define your hardware version
+MYHWVERSION=1.00.00
 
+# define your build manager
+MYBM="John Doe"
 
+# define your build manager email
+MYBMEMAIL="john.doe@example.com"
+
+# define syslog prefix
+LOGGERPREF="[ My SysInfo ] "
+
+# define the output file
+MYOUTFILE="/home/root/mysysinfo_report"
 
 ###################################
 #
@@ -31,4 +47,56 @@
 #
 ###################################
 
+
+function checkmyperms() 
+{
+
+	if [ $UID -ne 0 ]
+		then echo "This tool must be run as root..."
+		exit 
+	fi
+
+}
+
+
+function hwscanner()
+{
+
+# cpuinfo
+
+# meminfo
+
+# pci devices
+
+# usb devices
+
+# hw map
+
+}
+
+function swscanner()
+{
+
+# list rpm packages installed currently
+
+# list SW repositories this system is bound to
+
+# list flatpaks installed
+
+# list containers 
+
+}
+
+function myusersetup()
+{
+
+# list users
+
+# list groups
+
+
+}
+
+
+checkmyperms
 
